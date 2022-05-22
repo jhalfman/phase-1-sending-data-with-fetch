6c8d8fb9f79fb620f9c66e477e15102807158f5d
Jordan Halfman <jordanhalfman@gmail.com>
2022-05-21 19:17:07 -0700

    commit tree 5e61a81e7e1b253254080655f1518bb340408fe2

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    const newUser = {
+        name: name,
+        email: email,
+    }
+    console.log(newUser);
+    const newUserObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(newUser),
+    }
+    return fetch("http://localhost:3000/users", newUserObject)
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        document.body.append(data.id);
+    })
+    .catch(function (error) {
+        document.body.append(error.message);
+    });
+}
